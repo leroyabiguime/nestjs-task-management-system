2a44e12bdfd456007788d4944b76e6a6950f12be
arrow <arrow@192.168.1.10>
2021-10-03 18:54:46 +0000

    CRUD for tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,9 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe, Delete, Patch } from '@nestjs/common';
+import { UpdateDateColumn } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -10,10 +14,28 @@ export class TasksController {
     // getAllTasks(){
     // }
 
-    // @Get(':id')
-    // getTaskById(){
-    // }
+    @Get(':id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task>{
+        return this.tasksService.getTaskById(id);
+    }
+    
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksService.createTask(createTaskDto);
+    }
 
+    @Delete(':id')
+    deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.tasksService.deleteTask(id);
+    }
+
+    @Patch(':id/status')
+    updateTaskStatus(@Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
+    ): Promise<Task>{
+        return this.tasksService.updateTaskStatus(id, status);
+    }
     // @Post()
     // @UsePipes(ValidationPipe)
     // createTask(@Body() createTaskDto: CreateTaskDto): Task {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,14 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
 export class TasksService {
    constructor(
        @InjectRepository(TaskRepository)
-       private taskRepository: Repository<TaskRepository>
+       private taskRepository: Repository<Task>
    ) {}
+
+
+
+   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+       const task = new Task();
+       task.title = createTaskDto.title;
+       task.description = task.description;
+       task.status = TaskStatus.OPEN;
+       await task.save();
+       return task;
+   }
+
+
+   async getTaskById(id: number): Promise<Task>{
+    const found = await this.taskRepository.findOne(id)
+       if(!found) {
+           throw new NotFoundException(`Task with ID ${id} not found`);
+       }
+       return found;
+   }
+
+   async updateTaskStatus(id: number, status: TaskStatus) {
+       const task = await this.getTaskById(id);
+       task.status = status;
+       await task.save()
+       return task;
+   }
+   async deleteTask(id: number): Promise<void>{
+    const del = await this.taskRepository.delete(id);
+
+    if(del.affected === 0){
+        throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+   }
 
 }
  
